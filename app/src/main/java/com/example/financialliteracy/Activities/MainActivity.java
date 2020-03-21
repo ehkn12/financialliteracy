60adb9c0825efa4c4bbaf6fada0f2eb3dd9be0e4
Christopher <christopher.liu@hotmail.com.au>
2020-03-21 23:37:28 +1100

    Sprint 1
    Features complete

--- app/src/main/java/com/example/financialliteracy/Activities/MainActivity.java
+++ app/src/main/java/com/example/financialliteracy/Activities/MainActivity.java
@@ -2,16 +2,25 @@ package com.example.financialliteracy.Activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+<<<<<<< Updated upstream
 
 import android.content.Context;
+=======
+>>>>>>> Stashed changes
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+<<<<<<< Updated upstream
+=======
+import android.widget.EditText;
+import android.widget.TextView;
+>>>>>>> Stashed changes
 
 import com.example.financialliteracy.R;
 
 public class MainActivity extends AppCompatActivity {
+    Button tax;
 
     private Button temp;
 
@@ -19,6 +28,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+<<<<<<< Updated upstream
         temp = findViewById(R.id.button_temp);
 
         temp.setOnClickListener(new View.OnClickListener() {
@@ -35,6 +45,20 @@ public class MainActivity extends AppCompatActivity {
     public void launchInvestmentCalculatorActivity(View view) {
 
         Intent intent = new Intent(this, InvestmentCalculatorActivity.class);
+=======
+
+        tax = (Button) findViewById(R.id.tax);
+        tax.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                openTaxTalc();
+            }
+        });
+    }
+
+    public void openTaxTalc (){
+        Intent intent = new Intent(this, taxcalc.class);
+>>>>>>> Stashed changes
         startActivity(intent);
     }
 
